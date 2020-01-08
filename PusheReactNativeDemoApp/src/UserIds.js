d41dbedf9674a15be8d5db308c74b61b6de71cdd
Amir <amir.aleahmad@pushe.co>
2020-01-08 14:39:26 +0330

    Remove PusheDemo

--- PusheReactNativeDemoApp/src/UserIds.js
+++ PusheReactNativeDemoApp/src/UserIds.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
+import {View, TextInput, Button} from "react-native";
 import ScreenContainer from "./ScreenContainer";
 
 export default () => {
+    const [state, updateState] = useState({
+        email: '',
+    });
+
     return (
         <ScreenContainer>
-            
+            <View>
+
+            </View>
         </ScreenContainer>
     );
 };
